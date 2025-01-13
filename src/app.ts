80dda321d1b946f36cc415b8a550dcc5ca6a8d8a
cng04 <calebng.ca@gmail.com>
2025-01-12 21:03:41 -0500

    Updated PORT for deployment

--- src/app.ts
+++ src/app.ts
@@ -59,7 +59,7 @@ app.use('/judges-portal', judgesPortalRouter);
 
 module.exports = app;
 
-const PORT = process.env.DEV_PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
